d80522fc37a84bc62cac88dfdd0ee2fe684980e1
june6723 <june6723@naver.com>
2024-09-27 14:46:46 +0900

    feat: Streaming

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { fetchLatestInvoices } from '@/app/lib/data';
+import React from 'react';
 
-export default function page() {
+export default async function page() {
+  const latestInvoices = await fetchLatestInvoices();
+
   return <div>Invoices page</div>;
 }
